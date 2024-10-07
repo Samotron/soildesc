c6be8911615e447f5be6337574baacdbf315cd66
Sam Rogers <sam.rogers@atkinsrealis.com>
2024-10-07 22:10:58 +0100

    update project.clj

--- project.clj
+++ project.clj
@@ -1,7 +1,6 @@
-(defproject soildesc "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+(defproject soildesc "0.0.0"
+  :description "Work with soil descriptions"
   :url "http://example.com/FIXME"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :license {:name "LGPL-2.0-or-later"}
   :dependencies [[org.clojure/clojure "1.11.1"]]
   :repl-options {:init-ns soildesc.core})
